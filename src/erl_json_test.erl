c044f1f0e09c7255c07d69755a2ce5c043151e5c
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-03-18 20:33:11 +0100

    Bench in parallel to exercise lib latency variance

--- src/erl_json_test.erl
+++ src/erl_json_test.erl
@@ -3,18 +3,23 @@
 -define(RESULTS_FILE, "results.csv").
 -define(NUM_TESTS, 300).
 -define(PARSERS,
-        [{"jsone", fun jsone:encode/1, fun jsone:decode/1},
+        [
+         {"jsone", fun jsone:encode/1, fun jsone:decode/1},
          {"yawsjson2", fun json2:encode/1, fun json2:decode/1},
          {"jiffy", fun jiffy:encode/1, fun jiffy:decode/1},
          {"mochijson2", fun mochijson2:encode/1, fun mochijson2:decode/1},
-         {"jsx", fun jsx:encode/1, fun jsx:decode/1}]).
+         {"jsx", fun jsx:encode/1, fun jsx:decode/1}
+        ]).
 -define(TESTFILES,
-        [{"1x", "1x.json"},
+        [
+         {"1x", "1x.json"},
          {"3x", "3x.json"},
          {"9x", "9x.json"},
          {"27x", "27x.json"},
          {"81x", "81x.json"},
-         {"243x", "243x.json"}]).
+         {"243x", "243x.json"}
+        ]).
+-define(NPROC, 10).
 
 start() ->
     JSONs = [begin
@@ -27,17 +32,47 @@ start() ->
     _B = [ jiffy:encode(jiffy:decode(File)) || {_, File} <- JSONs],
     _C = [ mochijson2:encode(mochijson2:decode(File)) || {_, File} <- JSONs],
     _D = [ jsx:encode(jsx:decode(File)) || {_, File} <- JSONs],
-    ResultsDeep = [[begin
-                        T = {ParserName, TestName, size(JSON),
-                             bench(EncFun, DecFun, JSON)},
-                        io:format("~s ~s done~n", [ParserName, TestName]),
-                        T
-                    end
-                    || {TestName, JSON} <- JSONs]
-                   || {ParserName, EncFun, DecFun} <- ?PARSERS],
+    NProc = ?NPROC,
+    ResultsDeep = [ parallel(NProc,
+                             fun (I) ->
+                                     [ begin
+                                           T = {ParserName, TestName, size(JSON),
+                                                bench(EncFun, DecFun, JSON), I, NProc},
+                                           io:format("~s ~s done~n", [ParserName, TestName]),
+                                           T
+                                       end || {TestName, JSON} <- JSONs ]
+                             end)
+                    || {ParserName, EncFun, DecFun} <- ?PARSERS],
     Results = lists:flatten(ResultsDeep),
     format_results(Results),
     init:stop().
+
+parallel(NProc, BenchF) ->
+    Ref = make_ref(),
+    Self = self(),
+    Pids = [ spawn_link(fun () -> worker(Self, Ref, BenchF, I) end) || I <- lists:seq(1, NProc) ],
+    collect(Ref, Pids).
+
+worker(Parent, Ref, BenchF, I) ->
+    worker(Parent, Ref, BenchF, I, 1).
+
+worker(Parent, Ref, BenchF, I, N) ->
+    R = BenchF(I),
+    if
+        N == 1 -> Parent ! {results, Ref, R};
+        N /= 1 -> ok
+    end,
+    receive
+        stop -> Parent ! {stopped, Ref}
+        after 0 -> worker(Parent, Ref, BenchF, N + 1)
+    end.
+
+collect(Ref, Pids) ->
+    Results = [ receive {results, Ref, R} -> R end || _ <- Pids ],
+    [ P ! stop || P <- Pids ],
+    [ receive {stopped, Ref} -> ok end || _ <- Pids ],
+    io:format("all workers stopped\n", []),
+    Results.
 
 bench(EncFun, DecFun, TestJSON) ->
     DecThunk = fun() -> times(DecFun, TestJSON, ?NUM_TESTS) end,
@@ -51,13 +86,16 @@ format_results(Results) ->
                            "\"Test\","
                            "\"TestSize\","
                            "\"ResultEnc\","
-                           "\"ResultDec\"~n", []),
+                           "\"ResultDec\","
+                           "\"Proc\","
+                           "\"NProc\"~n", []),
     Out = [Header |
-           [io_lib:format("\"~s\",\"~s (~pb)\",~p,~p,~p~n",
+           [io_lib:format("\"~s\",\"~s (~pb)\",~p,~p,~p,~p,~p~n",
                           [Parser, Test, TestSize, TestSize,
                            round(ResultEnc / ?NUM_TESTS),
-                           round(ResultDec / ?NUM_TESTS)])
-            || {Parser, Test, TestSize, {ResultEnc, ResultDec}} <- Results]],
+                           round(ResultDec / ?NUM_TESTS),
+                           Proc, NProc])
+            || {Parser, Test, TestSize, {ResultEnc, ResultDec}, Proc, NProc} <- Results]],
     file:write_file(?RESULTS_FILE, lists:flatten(Out)).
 
 times(F, X,  0) -> F(X);
